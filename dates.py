eebcd25aff7b5660d1bcc25f4d3829aba7e2af80
cyKeddd <vrajmgupta@gmail.com>
2024-02-06 18:46:13 +0800

    added birthday eve

--- dates.py
+++ dates.py
@@ -66,11 +66,19 @@ print('Year: ' + str(current_date.year))
 
 # sometimes you receive the date as a string and need to convert it to a datetime object
 from datetime import datetime
+
 birthday = input('When is your birthday (dd/mm/yyyy)? ')
 birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
 print('Birthday: ' + str(birthday_date))
 
-
+# converting it to a datetime allows you to use the date functions
+from datetime import datetime, timedelta
+birthday = input('When is your birthday (dd/mm/yyyy)? ')
+birthday_date = datetime.strptime(birthday, '%d/%m/%')
+print('Birthday: ' + str(birthday_date))
+one_day = timedelta(days=1)
+birthday_eve = birthday_date - one_day
+print('Day before birthday: ' + str(birthday_eve))
 
 
 
